8b8241420be4b1c099898da1335d54a225239cf0
David <dschwartz.sf@gmail.com>
2014-11-04 13:12:02 -0800

    Added app icon, new original artwork, centered layout for portrait and landscape modes.

---
+++ Button1/AddLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddLocationViewController.h
+//  Button1
+//
+//  Created by David Schwartz on 11/4/14.
+//  Copyright (c) 2014 David Schwartz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddLocationViewController : UIViewController
+
+@end
